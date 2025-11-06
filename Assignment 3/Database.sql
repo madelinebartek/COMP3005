427e722f4d889b31428e7e3e1f51751ad4b8a489
madelinebartek <madelinecalgary@gmail.com>
2025-11-06 12:44:00 -0700

    Update Database.sql

--- Assignment 3/Database.sql
+++ Assignment 3/Database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE students(
-    student_id SERAIL PRIMARY KEY, 
+    student_id SERIAL PRIMARY KEY, 
     first_name TEXT NOT NULL, 
     last_name TEXT NOT NULL, 
     email TEXT NOT NULL UNIQUE,
@@ -9,4 +9,5 @@ CREATE TABLE students(
 INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
 ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
 ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+
 ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
