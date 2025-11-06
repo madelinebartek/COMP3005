27ac823cfe591abce7743863f57fef9b85677ad9
madelinebartek <madelinecalgary@gmail.com>
2025-11-06 12:44:39 -0700

    Update Database.sql

--- Assignment 3/Database.sql
+++ Assignment 3/Database.sql
@@ -9,5 +9,5 @@ CREATE TABLE students(
 INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
 ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
 ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
-
 ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
+
